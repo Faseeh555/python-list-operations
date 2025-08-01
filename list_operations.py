ce00177986035c12ee80855176d6cb39beb74fe4
Faseeh ur Rehman Anjum <146339103+Faseeh555@users.noreply.github.com>
2025-08-01 22:23:40 +0500

    Add files via upload

--- list_operations.py
+++ list_operations.py
@@ -1,1 +1,46 @@
-
+# how to make list
+# friends = ["Apple", "Orange", 5, 345.06, False, "Harry"]
+# print(friends)
+#Lists are mutable.
+# friends[3] = "Grapes"
+# print(friends)
+#
+# Add a single element at the end
+# list = [1,2,3]
+# list.append(4)
+# print(list)
+#
+# Insert at a specific position(index , element to insert)
+# list = [1,2,3]
+# list.insert(2, 'a')
+# print(list)
+#
+# Insert at end to extend list
+# list = [1,2,3]
+# list.extend([4, 5, 'six', 7])
+# print(list)
+#
+# Remove the first element
+# list = [1, 2, 3]
+# list.remove(1)
+# print(list)
+#
+# Remove by index(specific index targeting)
+# list = [1,2,3]
+# list.pop(1)
+# print(list)
+#
+# Remove by default index
+# list = [1,2,3]
+# list.pop() #it will remove the last one.
+# print(list)
+#
+# Delete the element at index using del function
+# list = [1,2,3,4,5]
+# del list[2]
+# print(list)
+#
+# Remove all elements.
+# list = [1,2,3,4,5]
+# list.clear()
+# print(list)
